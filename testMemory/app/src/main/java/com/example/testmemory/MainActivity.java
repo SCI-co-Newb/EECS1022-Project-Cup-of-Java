0e107b765d17231f9ce493b99c56e15d01a6fe58
Affan <affangazi0@gmail.com>
2022-03-15 12:27:43 -0400

    small update
    still stuck on the new grid stuff

--- testMemory/app/src/main/java/com/example/testmemory/MainActivity.java
+++ testMemory/app/src/main/java/com/example/testmemory/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.testmemory;
 
-import androidx.annotation.ContentView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.ColorStateList;
@@ -15,23 +14,27 @@ import java.util.Random; //uses random function
 import java.util.ArrayList; /*I'm using arraylist instead of arrays because this is easier.
                                 I have a python background and I think lists are more efficient for this project */
 
+
 public class MainActivity extends AppCompatActivity {
 
     private int level;
-    private int levelBuffer;
     private int square;
-    private int newGrid;
+    private int score;
+    private ArrayList<Button> selectList;
+    private boolean onoff;
 
     public MainActivity(){
         this.level = 0;
-        this.levelBuffer = 2;
         this.square = 0;
-        this.newGrid = 1;
+        this.score = 0;
+        this.selectList = new ArrayList<>();
+        this.onoff = false;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrayOfButtons();
     }
     public int randomSquare() {
         Random rando = new Random();
@@ -48,60 +51,45 @@ public class MainActivity extends AppCompatActivity {
                     B.setEnabled(false); }
                 //prevents users from clicking on something in the button array. This can be redundant idk
             }
-        }, 50); //This is 0.001 seconds after the screen/grid appears
+        }, 50); //This is 0.05 seconds after the screen/grid appears
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 block.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#cad4d1")));
                 for (Button B : buttonArray) {
-                    B.setEnabled(true); }
+                    if (B != block) {
+                        B.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#cad4d1"))); //to make sure all blocks are standard color
+                    }
+                    B.setEnabled(true); } //re enables the button's functionality
             }
         }, 800); //0.75 seconds later the color changes back to it's original color
         return block;
     }
-    public void Enforce() {
-        IncreaseLevel();
-        ArrayList<Button> selectList = new ArrayList<>();
-        selectList.add(selectSquare(arrayOfButtons()));
-        selectList.add(selectSquare(arrayOfButtons()));
-        selectList.add(selectSquare(arrayOfButtons()));
-        selectList.add(selectSquare(arrayOfButtons()));
-        selectList.add(selectSquare(arrayOfButtons()));
-         for (newGrid = 1; newGrid <= level; newGrid++) {
-             for (Button B : arrayOfButtons()) { //this for loop allows me to cycle through each button and set a on click listener
-                 B.setOnClickListener(new View.OnClickListener() { //Says to replace with lambda idk what that is
-                     @Override
-                     //I'm Making every button clickable
-                     public void onClick(View view) {
-                         if (selectList.contains(B)) {
-                             B.setBackgroundTintList(getColorStateList(android.R.color.white)); // changes color of button to white
-                             if (newGrid == level) {
-                                 newGrid = 0;
-                                 IncreaseLevel();
-                             }
-                             else {
-                                 newGrid++;
-                             }
-                         } else {
-                             level = 0; //sets level to 0 to restart
-                             levelBuffer = 0;
-                             newGrid = 1;
-                             setContentView(R.layout.activity_main); /*if it is the wrong button, the screen will go back to a fail screen
-                                                                   Currently it resets to main screen */
-                         }
-                     }
-                 });
-             }
-         }
-    }
-        /*if (newGrid == 0) {
-            IncreaseLevel();
-            for (Button B : arrayOfButtons()) {
-                B.setBackgroundTintList((ColorStateList.valueOf(Color.parseColor("#cad4d1"))));
-            }
-        } */
-    public ArrayList<Button> arrayOfButtons() {
 
+    public void Enforce() {
+        score = 0;
+        for (Button B : arrayOfButtons()) { //this for loop allows me to cycle through each button and set a on click listener
+            B.setOnClickListener(new View.OnClickListener() { //Says to replace with lambda idk what that is
+                @Override
+                //I'm Making every button clickable
+                public void onClick(View view) {
+                    if (selectList.contains(B)) {
+                        B.setBackgroundTintList(getColorStateList(android.R.color.white)); // changes color of button to white on click
+                        score++;
+                    } else {
+                        level = 0; //sets level to 0 to restart
+                        score = 0;
+                        selectList.clear();
+                        setContentView(R.layout.activity_main); /*if it is the wrong button, the screen will go back to a fail screen
+                                                                  Currently it resets to main screen */
+                    }
+                }
+            });
+        }
+    }
+
+
+    public ArrayList<Button> arrayOfButtons() {
         ArrayList<Button> arrayOfButtons = new ArrayList<>(); //I learned that arrays can have elements removed but the size of arrays can't be changed so i'm doing array list
         arrayOfButtons.add(findViewById(R.id.ButtonR1C1));
         arrayOfButtons.add(findViewById(R.id.ButtonR1C2));
@@ -135,14 +123,33 @@ public class MainActivity extends AppCompatActivity {
         level = level + 1; //level increases by 1
         TextView levelChange = findViewById(R.id.LEVELNUM);
         levelChange.setText(String.format("%d/25", level));
-
     }
+
     public void changeScreen(View view) { //changes start button on main screen to five by five grid
         setContentView(R.layout.fivebyfive);
-        Enforce();
+        while (!onoff) {
+            onoff = true;
+            IncreaseLevel();
+            for (int i = 1; i <= level; i++) {
+                selectList.add(selectSquare(arrayOfButtons()));
+            }
+            Enforce();
+            if (score == level){
+                for (Button B : arrayOfButtons()) {
+                    B.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#cad4d1")));
+                }
+                onoff = false;
+            }
+            if (level == 25) {
+                break;
+            }
+        }
     }
+
+
     public void goHome(View view) { //goes back to home screen
         setContentView(R.layout.activity_main);
         level = 0; //sets level back to zero when switched to home screen
+        selectList.clear();
     }
 }
